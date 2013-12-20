682345b667ba72762ef91fdb2c49e41dbfff839d
gdi2290 <github@gdi2290.com>
2013-12-19 21:48:58 -0800

    add $onRootScope

--- angular-event-emitter.js
+++ angular-event-emitter.js
@@ -1,3 +1,18 @@
 ;(function(module, angular, undefined) {
 'use strict';
+
+module.config(['$provide', function($provide) {
+  $provide.decorator('$rootScope', ['$delegate', function($delegate) {
+
+    Object.defineProperty($delegate.constructor.prototype, '$onRootScope', {
+      value: function(name, listener){
+        var unsubscribe = $delegate.$on(name, listener);
+        this.$on('$destroy', unsubscribe);
+      },
+      enumerable: false
+    });
+
+    return $delegate;
+  }]);
+}]);
 }(angular.module('angular-event-emitter', []), angular))
